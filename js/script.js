c2d901b44a6aab607d5a594850b1fa673854f643
cantaruttim <cantaruttim@outlook.com>
2024-01-29 22:01:21 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,72 @@
+const botaoPlayPause = document.getElementById('play-pause');
+const numeroCapitulos = 10;
+const audioCapitulo = document.getElementById('audio-capitulo');
+const botaoProxima = document.getElementById('proximo');
+const botaoVoltar = document.getElementById('anterior');
+const nomeCapitulo = document.getElementById('capitulo');
+let taTocando = 0;
+let capituloAtual = 1;
+
+
+function tocarFaixa() {
+    audioCapitulo.play();
+    botaoPlayPause.classList.remove('bi-play-circle-fill');
+    botaoPlayPause.classList.add('bi-pause-circle-fill');
+}
+
+function pausarFaixa() {
+    audioCapitulo.pause();
+    botaoPlayPause.classList.remove('bi-pause-circle-fill');
+    botaoPlayPause.classList.add('bi-play-circle-fill');
+}
+
+
+function tocarOuPausar() {
+
+    if ( taTocando === 0) {
+        tocarFaixa();
+        taTocando = 1;
+    } else {
+        pausarFaixa();
+        taTocando = 0;
+    }
+
+}
+
+function trocarNomeFaixa() {
+    nomeCapitulo.innerText = 'Capítulo ' + capituloAtual;
+}
+
+function proximaFaixa() {
+    
+    if (capituloAtual === numeroCapitulos) {
+        capituloAtual = 1;  
+    } else {
+        capituloAtual = capituloAtual + 1;
+    }
+
+    audioCapitulo.src = "./books/dom-casmurro/" + capituloAtual + '.mp3';
+    tocarFaixa();
+    taTocando = 1;
+    trocarNomeFaixa();
+
+}
+
+function voltarFaixa() {
+    
+    if (capituloAtual === 1) {
+        capituloAtual = numeroCapitulos;  
+    } else {
+        capituloAtual = capituloAtual - 1;
+    }
+
+    audioCapitulo.src = "./books/dom-casmurro/" + capituloAtual + '.mp3';
+    tocarFaixa();
+    taTocando = 1;
+    trocarNomeFaixa();
+
+}
+
+botaoPlayPause.addEventListener('click', tocarOuPausar);
+botaoProxima.addEventListener('click', proximaFaixa);
+botaoVoltar.addEventListener('click', voltarFaixa);
